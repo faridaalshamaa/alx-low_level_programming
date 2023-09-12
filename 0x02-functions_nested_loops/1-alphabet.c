ddcbbe5ab4083507b7605c534cf2cf84e7a6560d
farida <faridaalshamaa@gmail.com>
2023-09-12 16:10:58 -0700

    the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,16 +5,13 @@
  * Return: 0
  */
 
-int main(void)
+void print_alphabet(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar('c');
+	}
 	_putchar('\n');
-	return (0);
 }

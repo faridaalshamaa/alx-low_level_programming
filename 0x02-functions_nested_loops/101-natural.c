b29022df772c8119b5fe92f18107d616de634735
farida <faridaalshamaa@gmail.com>
2023-09-12 19:58:36 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,15 +7,16 @@
 
 int main(void)
 {
-int i, z = 0;
-while (i < 1024)
+	int i, z = 0;
 
-if ((i % 3 == 0) || (i % 5 == 0))
-{
-z += i;
-{
-i++;
-}
+	while (i < 1024)
+
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			z += i;
+		}
+		i++;
 }
 printf("%d\n", z);
 return (0);
